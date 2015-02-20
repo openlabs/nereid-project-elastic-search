4fdcd29d6502913b513d5be6ca43c8b3ea413d87
VaticanCameos <chakrabortypritish@gmail.com>
2015-02-20 12:05:56 +0530

    Boilerplate #3200

---
+++ nereid_project_elastic_search.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+    nereid_project_elastic_search.py
+
+    :copyright: (c) 2015 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
